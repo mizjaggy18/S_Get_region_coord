f1d6b0fd2f1bf38144ec95ef9f05a49657e09967
mizjaggy18 <77017051+mizjaggy18@users.noreply.github.com>
2023-10-06 03:22:38 +0000

    Update get_region_coord.py

--- get_region_coord.py
+++ get_region_coord.py
@@ -26,7 +26,7 @@ from shapely.geometry import Point, box
 from shapely import wkt
 
 import cytomine
-from cytomine.models import Property, Annotation, AnnotationTerm, AnnotationCollection, JobData, Job, TermCollection, ImageInstanceCollection
+from cytomine.models import Property, Annotation, AnnotationTerm, AnnotationCollection, JobData, Job, TermCollection, ImageInstanceCollection, ImageInstance
 
 
 def run(cyto_job, parameters):
@@ -42,74 +42,13 @@ def run(cyto_job, parameters):
     import_term = parameters.cytomine_import_term
     import_user = parameters.cytomine_import_user
 
-    # terms = TermCollection()
-    # # terms.project = project.id
-    # terms.fetch_with_filter("project", project.id)
-    # for term in terms:
-    #     print(term.id)
-    job.update(status=Job.RUNNING, progress=1, statusComment="Parameters gathered...")
-
-    # list_terms = []
-    # if parameters.cytomine_id_terms == 'all':
-    #     for term in terms:
-    #         list_terms.append(int(term.id))
-    # else:
-    #     list_terms = parameters.cytomine_id_terms
-    #     list_terms2 = list_terms.split(',')
-
-    # print('Input param:', parameters.cytomine_id_terms)
-    # print('Print list terms:', list_terms)
-    # print(type(list_terms))
-    # # list_imgs2 = list_terms.split(',')
-    # print(type(list_terms2))
-    # print('Print list terms2:', list_terms2)
-    # # for id_image in list_imgs2:
-    # #     print(id_image) 
-
+    job.update(status=Job.RUNNING, progress=5, statusComment="Parameters gathered...")
+    
+    terms = TermCollection().fetch_with_filter("project", project.id)    
     images = ImageInstanceCollection().fetch_with_filter("project", project.id)
-    job.update(status=Job.RUNNING, progress=2, statusComment="Terms and images gathered...")
-    
-    # print('images id:',images)
+    job.update(status=Job.RUNNING, progress=10, statusComment="Terms and images gathered...")    
 
-    # list_imgs = []
-    # if parameters.cytomine_id_images == 'all':
-    #     for image in images:
-    #         list_imgs.append(int(image.id))
-    # else:
-    #     list_imgs = parameters.cytomine_id_images
-    #     list_imgs2 = list_imgs.split(',')
-        
-    # print('Input param:', parameters.cytomine_id_images)
-    # print('Print list images:', list_imgs)
-    # print(type(list_imgs))
-    # # list_imgs2 = list_imgs.split(',')
-    # print(type(list_imgs2))
-    # print('Print list images2:', list_imgs2)
-    # # for id_image in list_imgs2:
-    # #     print(id_image) 
-
-    #Go over images
-    # for (i, id_image) in enumerate(list_imgs2):  
-    
-    # for (i,id_term) in enumerate(list_terms2):
-    progress=4
-    
-
-    annotations = AnnotationCollection()
-    annotations.image = id_image
-    # annotations.term = id_terms
-    annotations.project = project.id
-    annotations.showWKT = True
-    annotations.showMeta = True
-    annotations.showGIS = True
-    annotations.showTerm = True
-    annotations.fetch()
-
-    # print(id_image)
-    # print(id_terms)
-    # print(project.id)
-
-    # Get the list of annotations
+    # Get the list of annotations to import
     annotations_import = AnnotationCollection(
         terms=[import_term],
         image=import_image,
@@ -122,44 +61,25 @@ def run(cyto_job, parameters):
         showImage=True,
         includeAlgo=True
     ).fetch()
-#     annotations_import = AnnotationCollection()
-#     annotations_import.image = import_image #id_image
-#     annotations_import.term = import_term #id_term
-#     annotations_import.project = import_project #project.id
-#     annotations_import.showWKT = True
-#     annotations_import.showMeta = True
-#     annotations_import.showGIS = True
-#     annotations_import.showTerm = True
-#     annotations_import.fetch()
-    print("Total annotations: ",len(annotations_import))
-    print(annotations_import)
+
+    print("Total annotations to import: ",len(annotations_import))
+    
+    annotations = AnnotationCollection()
+
     # progress_delta=100-(progress)/len(annotations_import)
 
-    # print(import_image)
-    # print(import_term)
-    # print(import_project)
-
     for anno in annotations_import:
-        roi_geometry = wkt.loads(anno.location)
-        Annotation(
-        location=roi_geometry.wkt,
-        id_image=id_image,
-        id_project=project.id,
-        id_terms=[id_terms]).save()
-    #     annotations.append(Annotation(
-    #         location=roi_geometry.wkt,
-    #         id_image=id_image,
-    #         id_project=project.id,
-    #         id_terms=[id_terms]))
-    #     print(".",end = '',flush=True)        
-    # annotations.save()
+        roi_geometry = wkt.loads(anno.location)        
+        annotations.append(Annotation(
+            location=roi_geometry.wkt,
+            id_image=id_image,
+            id_project=project.id,
+            id_terms=[id_terms]))   
+    annotations.save()
+
         # annotation_import = Annotation(location=roi_geometry.wkt, id_image=id_image, id_project=project.id).save()
         # # print(annotation_import.id)
-        # AnnotationTerm(annotation_import.id, [id_terms]).save()
-        
-    
-
-
+        # AnnotationTerm(annotation_import.id, [id_terms]).save()  
         # job.update(status=Job.RUNNING, progress=progress, statusComment="Copying region coordinates....")
             # annotation.delete()
                 
